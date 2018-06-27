31e4df99e31f8ef18032f72e9b34c47b8a45b479
faisal5170 <faisalmpathan@gmail.com>
2018-06-27 18:14:28 +0530

    DataTable with Linq

--- ServerSidePaginationWithDataTableMVC/Controllers/OrdersController.cs
+++ ServerSidePaginationWithDataTableMVC/Controllers/OrdersController.cs
@@ -224,5 +224,133 @@ namespace ServerSidePaginationWithDataTableMVC.Controllers
             }
             base.Dispose(disposing);
         }
+
+        public ActionResult Index1()
+        {
+
+            return View();
+        }
+
+
+        public JsonResult GetOrdersWithDT(JQueryDataTableParamModel param)
+        {
+            var sortColumnIndex = Convert.ToInt32(Request["iSortCol_0"]);
+            var sortDirection = Request["sSortDir_0"]; // asc or desc
+            var totalrecored = db.Orders.Count();
+            var data = new List<Order>();
+            if (param.sSearch == "" && param.sSearch == null)
+            {
+                data = db.Orders.OrderBy(a => a.OrderId)
+                    //.Skip(param.iDisplayStart)
+                    //.Take(param.iDisplayLength)
+                    .ToList();
+            }
+            else
+            {
+                data = db.Orders.OrderBy(a => a.OrderId)
+                    .Where(r => r.OrderId.ToString() == param.sSearch || r.OrderNumber.ToString() == param.sSearch || (r.InternalNotes != null ? r.InternalNotes.Contains(param.sSearch) : r.InternalNotes == param.sSearch))
+                    //.Skip(param.iDisplayStart)
+                    //.Take(param.iDisplayLength)
+                    .ToList();
+            }
+            var isOrderIdSortable = Convert.ToBoolean(Request["bSortable_0"]);
+            var isOrderNumberSortable = Convert.ToBoolean(Request["bSortable_1"]);
+            var isCustomerInstructionsSortable = Convert.ToBoolean(Request["bSortable_2"]);
+            var isOrderTotalSortable = Convert.ToBoolean(Request["bSortable_3"]);
+            var isOrderDateSortable = Convert.ToBoolean(Request["bSortable_4"]);
+
+
+            Func<Order, string> orderingFunction = (c =>
+                sortColumnIndex == 0 && isOrderIdSortable ? c.OrderId.ToString() :
+                sortColumnIndex == 1 && isOrderNumberSortable ? c.OrderNumber.ToString() :
+                sortColumnIndex == 2 && isCustomerInstructionsSortable ? c.CustomerInstructions.ToString() :
+                sortColumnIndex == 3 && isOrderTotalSortable ? c.OrderTotal.ToString() :
+                sortColumnIndex == 4 && isOrderDateSortable ? c.OrderDate.ToString() : "");
+
+            var sortedResult = new List<Order>();
+            if (sortDirection == "asc")
+                sortedResult = data.OrderBy(orderingFunction).Skip(param.iDisplayStart).Take(param.iDisplayLength).ToList();
+            else
+                sortedResult = data.OrderByDescending(orderingFunction).Skip(param.iDisplayStart).Take(param.iDisplayLength).ToList();
+
+
+            var result = from c in sortedResult
+                         select new[]
+                         {
+                          Convert.ToString( c.OrderId ),  // 0     
+                          Convert.ToString( c.OrderNumber ),  // 1     
+                          Convert.ToString( c.CustomerInstructions ),  // 2     
+                          Convert.ToString( c.OrderTotal ),  // 3     
+                          Convert.ToString( c.OrderDate ),  // 4    
+                                                   };
+            return Json(new
+            {
+                aaData = result,
+                iTotalRecords = totalrecored,
+                iTotalDisplayRecords = totalrecored
+            }, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetOrdersWithDT1()
+        {
+            JsonResult result = new JsonResult();
+            try
+            {
+                // Initialization.   
+                string search = Request.Form.GetValues("search[value]")[0];
+                string draw = Request.Form.GetValues("draw")[0];
+                string order = Request.Form.GetValues("order[0][column]")[0];
+                string orderDir = Request.Form.GetValues("order[0][dir]")[0];
+                int startRec = Convert.ToInt32(Request.Form.GetValues("start")[0]);
+                int pageSize = Convert.ToInt32(Request.Form.GetValues("length")[0]);
+                // Loading.   
+                List<Order> data = db.Orders.ToList();
+                // Total record count.   
+                int totalRecords = data.Count;
+                // Verification.   
+                if (!string.IsNullOrEmpty(search) &&
+                    !string.IsNullOrWhiteSpace(search))
+                {
+                    // Apply search   
+                    data = data.Where(p => p.OrderId.ToString().ToLower().Contains(search.ToLower()) ||
+                        p.OrderNumber.ToString().Contains(search.ToLower()) ||
+                        p.OrderDate.ToString().Contains(search.ToLower()) ||
+                        p.OrderTotal.ToString().Contains(search.ToLower())
+                     ).ToList();
+                }
+                //Sorting.
+                //if (!(string.IsNullOrEmpty(order) && string.IsNullOrEmpty(orderDir)))
+                //{
+                //    data = data.OrderBy(order + " " + orderDir).ToList();
+                //}
+                int recFilter = data.Count;
+                data = data.Skip(startRec).Take(pageSize).ToList();
+                var modifiedData = data.Select(d =>
+                    new
+                    {
+                        d.OrderId,
+                        d.OrderNumber,
+                        d.CustomerInstructions,
+                        d.OrderDate,
+                        d.OrderTotal
+                    }
+                    );
+                // Loading drop down lists.   
+                result = this.Json(new
+                {
+                    draw = Convert.ToInt32(draw),
+                    recordsTotal = totalRecords,
+                    recordsFiltered = recFilter,
+                    data = modifiedData
+                }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                // Info   
+                Console.Write(ex);
+            }
+            // Return info.   
+            return result;
+        }
     }
 }
